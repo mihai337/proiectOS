6d198ea5e20dd62c2a84aac13bbc800199039cbc
mihai337 <constantinm7787@gmail.com>
2024-03-26 08:01:50 -0500

    afisare foldere recursiv

--- main.c
+++ main.c
@@ -1,35 +1,55 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <string.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void parse(DIR *dirp){
+
+void parse(DIR *dirp, char* path ,int nr_tabs){
+    
+    struct stat *st = (struct stat*)malloc(sizeof(struct stat));
     struct dirent *r;
-    struct stat *st;
     r = readdir(dirp);
     if(r == NULL){
         return;
     }
     else{
-        printf("%s" , r->d_name);
-        // stat(r->d_ino,st);
+        char *init_path = malloc((strlen(path)+3) * sizeof(char));
+        strcat(path,"/");
+        strcpy(init_path,path);
+        path = realloc(path , (strlen(path) + strlen(r->d_name) + 1)*sizeof(char));
+        strcat(path,r->d_name);
+
+        int result = stat(path , st);
+        for(int i=0;i<=nr_tabs;i++)
+            printf("  ");
+        printf("%s\n" ,r->d_name);
+        if(S_ISDIR(st->st_mode) && strcmp(r->d_name,".") && strcmp(r->d_name,"..") ){
+            DIR *new_dir = opendir(path);
+            if(new_dir != NULL){
+                parse(new_dir,path,nr_tabs+1);
+                closedir(new_dir);
+            }
+        }
+        parse(dirp,init_path,nr_tabs);
     }
 }
 
 int main(int argc , char **argv){
-    if(argc != 2){
-        return 0;
-    }
-
-    DIR *dirp = opendir(argv[1]);
+    // if(argc != 2){
+    //     return 0;
+    // }
+    char *path = strdup("/home/mihai/Desktop/");
+    DIR *dirp = opendir(path);
 
     if(dirp == NULL){
         perror("un merge");
         exit(1);
     }
-    parse(dirp);
+    parse(dirp,path,0);
+    closedir(dirp);
     return 0;
 }
