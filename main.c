79b70c9eedbe6374ef005bc9ab98c6fd7c72e5c3
mihai337 <138067650+mihai337@users.noreply.github.com>
2024-04-02 09:36:06 +0300

    Update main.c

--- main.c
+++ main.c
@@ -6,9 +6,17 @@
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
+struct Snapshot{
+    char name[256];
+    int size;
+    char location[300];
+    ino_t inode;
+    time_t last_modified;
+}file[1000];
 
-void parse(DIR *dirp, char* path ,int nr_tabs){
+void parse(DIR *dirp, char* path,int index){
     
     struct stat *st = (struct stat*)malloc(sizeof(struct stat));
     struct dirent *r;
@@ -17,26 +25,27 @@ void parse(DIR *dirp, char* path ,int nr_tabs){
         return;
     }
     else{
-        // char *init_path = malloc((strlen(path)+3) * sizeof(char));
-        // strcat(path,"/");
-        // strcpy(init_path,path);
-        // path = realloc(path , (strlen(path) + strlen(r->d_name) + 1)*sizeof(char));
-        // strcat(path,r->d_name);
+        int result = stat(path , st); //not the path of the file
+        printf("%s\n" , path);
+        file[index].inode = st->st_ino;
+        strcpy(file[index].name , r->d_name);
+        strcpy(file[index].location , path);
+        strcat(file[index].location , "/");
+        strcat(file[index].location , file[index].name);
+        file[index].last_modified = st->st_mtime;
+        // printf("%d\n"  ,st->st_size);
+        file[index].size = st->st_size;
 
-        int result = stat(path , st);
-        for(int i=0;i<=nr_tabs;i++)
-            printf("  ");
-        printf("%s\n" ,r->d_name);
         if(S_ISDIR(st->st_mode) && strcmp(r->d_name,".") && strcmp(r->d_name,"..") ){
             char *new_path = malloc((strlen(path)+strlen(r->d_name)+2)*sizeof(char));
             sprintf(new_path,"%s%s/",path,r->d_name);
             DIR *new_dir = opendir(new_path);
             if(new_dir != NULL){
-                parse(new_dir,path,nr_tabs+1);
+                parse(new_dir,path,index+1);
                 closedir(new_dir);
             }
         }
-        parse(dirp,path,nr_tabs);
+        parse(dirp,path,index+1);
     }
 }
 
@@ -48,10 +57,16 @@ int main(int argc , char **argv){
     DIR *dirp = opendir(argv[1]);
 
     if(dirp == NULL){
-        perror("un merge");
+        perror("nu merge");
         exit(1);
     }
+
     parse(dirp,argv[1],0);
     closedir(dirp);
+
+    for(int i=0;i<5;i++){
+        printf("%s  %d\n" , file[i].name , file[i].size);
+    }
+
     return 0;
 }
