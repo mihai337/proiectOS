2e3536bec16a06560f26f8a7978681824288114c
mihai337 <constantinm7787@gmail.com>
2024-04-15 20:54:14 -0500

    ceva ceva

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/stat.h>
 #include <string.h>
 #include <dirent.h>
@@ -16,57 +17,132 @@ struct Snapshot{
     time_t last_modified;
 }file[1000];
 
-void parse(DIR *dirp, char* path,int index){
-    
-    struct stat *st = (struct stat*)malloc(sizeof(struct stat));
-    struct dirent *r;
-    r = readdir(dirp);
-    if(r == NULL){
-        return;
-    }
-    else{
-        int result = stat(path , st); //not the path of the file
-        printf("%s\n" , path);
-        file[index].inode = st->st_ino;
-        strcpy(file[index].name , r->d_name);
-        strcpy(file[index].location , path);
-        strcat(file[index].location , "/");
-        strcat(file[index].location , file[index].name);
-        file[index].last_modified = st->st_mtime;
-        // printf("%d\n"  ,st->st_size);
-        file[index].size = st->st_size;
 
-        if(S_ISDIR(st->st_mode) && strcmp(r->d_name,".") && strcmp(r->d_name,"..") ){
-            char *new_path = malloc((strlen(path)+strlen(r->d_name)+2)*sizeof(char));
-            sprintf(new_path,"%s%s/",path,r->d_name);
-            DIR *new_dir = opendir(new_path);
-            if(new_dir != NULL){
-                parse(new_dir,path,index+1);
-                closedir(new_dir);
-            }
+void parse(DIR *dirp,char *path,int level){
+    struct dirent *dp;
+    struct stat buf;
+    char *new_path = (char *)malloc(5000);
+    while((dp = readdir(dirp)) != NULL){
+        if(strcmp(dp->d_name,".") == 0 || strcmp(dp->d_name,"..") == 0){
+            continue;
         }
-        parse(dirp,path,index+1);
+        strcpy(new_path,path);
+        strcat(new_path,dp->d_name);
+        stat(new_path,&buf);
+        strcpy(file[level].name,dp->d_name);
+        file[level].size = buf.st_size;
+        strcpy(file[level].location,new_path);
+        file[level].inode = buf.st_ino;
+        file[level].last_modified = buf.st_mtime;
+        level++;
+        if(S_ISDIR(buf.st_mode)){
+            strcat(new_path,"/");
+            DIR *new_dir = opendir(new_path);
+            parse(new_dir,new_path,level);
+            closedir(new_dir);
+        }
     }
 }
 
+// void parse(DIR *dirp, char *dirname, int file_count) {
+//     struct dirent *entry;
+//     struct stat statbuf;
+//     char path[5000];
+
+//     while ((entry = readdir(dirp)) != NULL) {
+//         if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0)
+//             continue;
+        
+//         snprintf(path, sizeof(path), "%s/%s", dirname, entry->d_name);
+
+//         if (lstat(path, &statbuf) == -1) {
+//             perror("lstat");
+//             continue;
+//         }
+
+//         // Populate file_info struct with information about the file
+//         strncpy(file[file_count].name, entry->d_name, sizeof(file[file_count].name));
+//         file[file_count].size = statbuf.st_size;
+//         strcpy(file[file_count].location, path);
+//         file[file_count].inode = statbuf.st_ino;
+//         file[file_count].last_modified = statbuf.st_mtime;
+
+//         // Increment file count
+//         file_count++;
+
+//         // Process file or directory
+//         if (S_ISDIR(statbuf.st_mode)) {
+//             DIR *subdir = opendir(path);
+//             if (subdir != NULL) {
+//                 parse(subdir, path, file_count);
+//                 closedir(subdir);
+//             }
+//         }
+//     }
+// }
+
+void create_snapshot(){
+    //copy child process
+}
+
 int main(int argc , char **argv){
-    if(argc != 2){
+    int i;
+    if(argc < 2){
         return 0;
     }
-    // char *path = strdup("/home/mihai/Desktop/");
-    DIR *dirp = opendir(argv[1]);
 
-    if(dirp == NULL){
-        perror("nu merge");
-        exit(1);
+    for(i=0;i<1000;i++){
+        file[i].size = -1;
+    }
+    
+    int child_index = 0;
+    for(i=1;i<argc;i++){
+        child_index++;
+        pid_t pid = fork();
+        if(pid == -1){
+            perror("fork error");
+            exit(1);
+        }
+        else if(pid == 0){
+            //child process
+            DIR *dirp = opendir(argv[i]);
+
+            if(dirp == NULL){
+                perror("opendir error");
+                exit(1);
+            }
+
+            parse(dirp,argv[i],0);
+            closedir(dirp);
+            printf("%s\n",argv[i]);
+            //save snapshot
+            char* snap_location = (char *)malloc(5000);
+            strcpy(snap_location,argv[i]);
+            strcat(snap_location,"snapshot.txt");
+            FILE *f = fopen(snap_location,"w");
+
+            if(f == NULL){
+                perror("fopen error");
+                exit(1);
+            }
+
+            for(int j=0;j<1000;j++){
+                // if(file[j].size == -1){
+                //     break;
+                // }
+                fprintf(f,"%s %d %s %ld %ld\n",file[j].name,file[j].size,file[j].location,file[j].inode,file[j].last_modified);
+            }
+            fclose(f);
+            free(snap_location);
+            exit(0);
+            // wait(NULL);
+            }
     }
 
-    parse(dirp,argv[1],0);
-    closedir(dirp);
-
-    for(int i=0;i<5;i++){
-        printf("%s  %d\n" , file[i].name , file[i].size);
+    for(i=1;i<argc;i++){
+        wait(NULL);
     }
+    
 
     return 0;
 }
